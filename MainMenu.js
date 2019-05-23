100e0bf31c3f3ba29975ee3c978b4e801a001612
Nicholas Kissaun <31455235+Tephrite@users.noreply.github.com>
2019-05-23 14:17:38 +0100

    Add files via upload

--- MainMenu.js
+++ MainMenu.js
@@ -8,14 +8,22 @@ var mainMenuState = new Phaser.Class({
   
     preload: function() {
         this.load.image('bg', 'Assets/Pictures/Menu.png');
-        //this.load.image('playButton', 'Assets/Pictures/button_play.png', 193, 71);
-        this.load.image('exitButton', 'Assets/Pictures/button_exit.png');
+        this.load.image('playButton', 'Assets/Pictures/button_play.png', 193, 71);
+        this.load.image('exitButton', 'Assets/Pictures/button_exit.png', 193, 71);
     },
 
     create: function() {
         console.log("MainMenu");
         this.add.image(640, 400, 'bg');
-        //var button = this.add.button(95, 200, 'playButton', function(){ console.log('play');}, this,0,0,0)
+        
+        let playButton = this.add.image(640, 300, 'playButton');
+        playButton.setInteractive();
+        playButton.on('pointerdown', function(pointer){ game.scene.load('GamePlay')});
+        
+        let exitButton = this.add.image(640, 500, 'exitButton');
+        exitButton.setInteractive();
+        exitButton.on('pointerdown', function(pointer){ window.close();});
+        
         game.scene.start('GamePlay');
     },
 
