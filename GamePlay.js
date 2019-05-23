cd660393617b805743f9a7c52ebd5c7d512dde86
CaptainJush <39602652+CaptainJush@users.noreply.github.com>
2019-05-23 17:39:15 +0100

    Added Castle

--- GamePlay.js
+++ GamePlay.js
@@ -11,6 +11,8 @@ var gamePlayState = new Phaser.Class({
 
         this.load.image('ui', 'Assets/Pictures/UI.png');
         this.load.image('bg', 'Assets/Pictures/Map_1.png');
+        this.load.image('city', 'Assets/Pictures/Clickergamecastle.png')
+        
         
         this.load.audio('bgMusic', 'Assets/Music/background sound/beethoven_symphony_5_1.ogg');
     },
@@ -30,12 +32,17 @@ var gamePlayState = new Phaser.Class({
         vHP1 = this.add.text(700, 10, '1').setFontFamily('Arial').setFontSize(48).setColor('#ffff00');
         vHP2 = this.add.text(960, 10, '1').setFontFamily('Arial').setFontSize(48).setColor('#ffff00');
         round = this.add.text(1200, 10, '1').setFontFamily('Arial').setFontSize(48).setColor('#ffff00');
+        this.add.sprite(280, 280, 'city').setDisplaySize(128, 128);
         
+     
         
+        var music= this.sound.add('bgMusic');
+        music.play();
     },
 
     update: function() {
         
+        // Update objects & variables
     }
 });
 
